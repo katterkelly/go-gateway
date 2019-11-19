ba48ccc6eb2fc3547c57803cc1d96aadf9ccf9c7
Tiny.VN Laptop <tranhuyen.trang181@gmail.com>
2019-11-20 01:07:55 +0700

    - Update version 2.5.4 - yêu cầu quyền admin main.exe - Only ipv4 (route -4 print 0.0.0.0) - Set nodelay conn socks - User connect sẵn vào tiny(vietnam), luôn luôn fake ip web tiny.vn

--- gateway_windows.go
+++ gateway_windows.go
@@ -7,7 +7,7 @@ import (
 )
 
 func DiscoverGateway() (ip net.IP, ip1 net.IP, err error) {
-	routeCmd := exec.Command("route", "print", "0.0.0.0")
+	routeCmd := exec.Command("route", "-4", "print", "0.0.0.0")
 	routeCmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
 	output, err := routeCmd.CombinedOutput()
 	if err != nil {
